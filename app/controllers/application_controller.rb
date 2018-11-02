167bea32280e2d62fa83525939d37ec242711014
williamLin <william.lin@sun-innovation.com>
2018-11-02 16:10:36 +0800

    feat. User登入與註冊功能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
-	protect_from_forgery
+  before_action :authenticate_user!
+  protect_from_forgery
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+  end
 end
