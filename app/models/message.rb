c0738823803added2f1f65eed068d7ff4cdaa7cc
williamLin <william.lin@sun-innovation.com>
2018-11-05 17:25:01 +0800

    fix. fix association error

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,17 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
-  after_create_commit { BroadCastMessageJob.perform_later self }
+  after_create_commit :broadcast_message
+  after_create_commit :create_participant_record
+
+  private
+
+  def broadcast_message
+  	BroadCastMessageJob.perform_later self
+  end
+
+  def create_participant_record
+  	return if user.chatrooms.include?(chatroom)
+  	user.participants.create(chatroom_id: self.chatroom.id)
+  end
 end
