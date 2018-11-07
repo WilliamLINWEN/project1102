72f94869624574e8b92789d390ea8f820164a191
RuBeginner <kaokaolin456@gmail.com>
2018-11-07 23:38:11 +0800

    feat. user create avatar after created

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,22 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :participants
   has_many :messages
 
+  after_create_commit :assign_avatar
+
   def recently_attended_rooms
   	chatrooms.where(id: messages.order(created_at: :desc).limit(5).collect(&:chatroom_id))
+  end
+
+  private
+
+  def assign_avatar
+    samples = ['identicon', 'monsterid', 'wavatar', 'retro', 'robohash']
+    begin
+      image_url = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=' + samples[Random.new.rand(samples.length - 1)]
+    rescue => e
+      image_url = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp'
+    end
+
+    self.update(avatar: image_url)
   end
 end
